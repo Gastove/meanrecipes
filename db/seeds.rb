f1d12cba015253cba16c38d064a7123d41203d64
Ross Donaldson <ross@selfawaregames.com>
2013-08-13 14:31:15 -0700

    Adding Controllers: 	* ingredients_controller.rb 	* directions_controller.rb
    Adding migrations for:
           * CreateRecipes
           * CreateIngredients
           * CreateDirections
    
    Adding seed data; minor bug fixes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+test_recipe = {
+  :recipe_name => "Mean Manhattan",
+  :ingredients => [
+                   {
+                     :name => "rye whiskey",
+                     :quantity => "8 parts"
+                   },
+                   {
+                     :name => "vermouth",
+                     :quantity => "3 parts"
+                   },
+                   {
+                     :name => "Marasca Cherry",
+                     :quantity => 1
+                   },
+                   {
+                     :name => "Angostura Bitters",
+                     :quantity => "To Taste"
+                   }
+                  ],
+  :directions => [
+                  {
+                    :ordinality => 1, 
+                    :body => "Fill Pint Glass with Ice"
+                  },
+                  {
+                    :ordinality => 2,
+                    :body => "Add all liquid ingredients"
+                  },
+                  {
+                    :ordinality => 3,
+                    :body => "Stir until exterior of glass is very cold"
+                  },
+                  {
+                    :ordinality => 4,
+                    :body => "Pour into rocks glass"
+                  },
+                  {
+                    :ordinality => 5,
+                    :body => "Garnish with Cherry"
+                  }
+                 ]
+}
+
+Recipe.create(test_recipe)
+
