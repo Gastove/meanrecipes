def8d0a05dc7fe4821f76378262e901a1b4625a7
Lauren <eastridgele@gmail.com>
2013-08-13 13:38:52 -0700

    little bug fixes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,13 @@
 
 class RecipesController < ApplicationController
 
+
   def index
-    @recipes = Recipes.all
+    @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:name])
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -14,56 +15,57 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(params)
+    @recipe = Recipe.new(params[:recipe])
     @recipe.save
   end
 
-  def generate_test_data
-    test_recipe = {
-      :recipe_name => "Mean Manhattan",
-      :ingredients => [
-        {
-          :name => "rye whiskey",
-          :quantity => "8 parts"
-          },
-        {
-          :name => "vermouth",
-          :quantity => "3 parts"
-          },
-        {
-          :name => "Marasca Cherry",
-          :quantity => 1
-          },
-        {
-          :name => "Angostura Bitters",
-          :quantity => "To Taste"
-          }
-        ],
-      :directions => [
-                      {
-                        :ordinality => 1, 
-                        :text => "Fill Pint Glass with Ice"
-                      },
-                      {
-                        :ordinality => 2,
-                        :text => "Add all liquid ingredients"
-                      },
-                      {
-                        :ordinality => 3,
-                        :text => "Stir until exterior of glass is very cold"
-                      },
-                      {
-                        :ordinality => 4,
-                        :text => "Pour into rocks glass"
-                      },
-                      {
-                        :ordinality => 5,
-                        :text => "Garnish with Cherry"
-                      }
-                     ]
-    }
-    test = Recipe.create(test_recipe)
-  
-  end
+  # this should go in seeds.rb... also check out a gem called faker for making obscene quantities of test data
+  # def generate_test_data
+  #   test_recipe = {
+  #     :recipe_name => "Mean Manhattan",
+  #     :ingredients => [
+  #       {
+  #         :name => "rye whiskey",
+  #         :quantity => "8 parts"
+  #         },
+  #       {
+  #         :name => "vermouth",
+  #         :quantity => "3 parts"
+  #         },
+  #       {
+  #         :name => "Marasca Cherry",
+  #         :quantity => 1
+  #         },
+  #       {
+  #         :name => "Angostura Bitters",
+  #         :quantity => "To Taste"
+  #         }
+  #       ],
+  #     :directions => [
+  #                     {
+  #                       :ordinality => 1,
+  #                       :text => "Fill Pint Glass with Ice"
+  #                     },
+  #                     {
+  #                       :ordinality => 2,
+  #                       :text => "Add all liquid ingredients"
+  #                     },
+  #                     {
+  #                       :ordinality => 3,
+  #                       :text => "Stir until exterior of glass is very cold"
+  #                     },
+  #                     {
+  #                       :ordinality => 4,
+  #                       :text => "Pour into rocks glass"
+  #                     },
+  #                     {
+  #                       :ordinality => 5,
+  #                       :text => "Garnish with Cherry"
+  #                     }
+  #                    ]
+  #   }
+  #   test = Recipe.create(test_recipe)
+
+  # end
 
 end
